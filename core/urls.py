de860bc6fa99483243a2b30c0be05c9186e75bb6
kodexArg <gcavedal@gmail.com>
2025-06-16 20:38:06 -0300

    v0.0.1: Esqueleto inicial del proyecto con URLs organizadas bajo /core/

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import health, db_health_check, home, hello_world, htmx_demo, login_view, logout_view, dashboard_view
 
 urlpatterns = [
+    path('', home, name='home'),
     path('health/', health, name='health'),
     path('health/db/', db_health_check, name='db_health_check'),
     path('home/', home, name='home'),
